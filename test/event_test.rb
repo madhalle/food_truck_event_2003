c677ba43870b323eb8eb0d8741098899e5e2f3b4
Mad Hallé <mad.halle@icloud.com>
2020-04-15 11:12:27 -0600

    total inventory test of event added

--- test/event_test.rb
+++ test/event_test.rb
@@ -16,6 +16,7 @@ class EventTest < Minitest::Test
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
   end
 
   def test_it_exists
@@ -77,9 +78,58 @@ class EventTest < Minitest::Test
     assert_equal expected, @event.food_trucks_that_sell(@item1)
     assert_equal [@food_truck2], @event.food_trucks_that_sell(@item4)
   end
+  # Add a method to your `Event` class called `sorted_item_list` that returns a list of names of all items the FoodTrucks have in stock, sorted alphabetically. This list should not include any duplicate items.
+  #
+  # Additionally, your `Event` class should have a method called `total_inventory` that reports the quantities of all items sold at the event. Specifically, it should return a hash with items as keys and hash as values - this sub-hash should have two key/value pairs: quantity pointing to total inventory for that item and food_trucks pointing to an array of the food trucks that sell that item.
+  #
+  # You `Event` will also be able to identify `overstocked_items`.  An item is overstocked if it is sold by more than 1 food truck AND the total quantity is greater than 50.
+  #
+  # Use TDD to update your `Event` class so that it responds to the following interaction pattern:
+  #
+
+  def test_event_total_inventory
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+
+    @food_truck3.stock(@item1, 65)
+    @food_truck3.stock(@item3, 10)
+
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expected = {
+        @item1 => {
+          quantity: 100,
+          food_trucks: [@food_truck1, @food_truck3]
+        },
+        @item2 => {
+          quantity: 7,
+          food_trucks: [@food_truck1]
+        },
+        @item4 => {
+          quantity: 50,
+          food_trucks: [@food_truck2]
+        },
+        @item3 => {
+          quantity: 35,
+          food_trucks: [@food_truck2, @food_truck3]
+        },
+      }
+    assert_equal expected, @event.total_inventory
+  end
+
+
+  #
+  # pry(main).overstocked_items
+  # #=> [#<Item:0x007f9c56740d48...>]
+  #
+  # pry(main)> event.sorted_item_list
+  # #=> ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
+  # ```
 
 
 end
-# A FoodTruck will be able to calculate their `potential_revenue` -
-# the sum of all their items' price * quantity.
-#
