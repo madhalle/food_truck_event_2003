67561917998196a31214984b9b7fda6907242bca
Mad Hallé <mad.halle@icloud.com>
2020-04-15 10:46:35 -0600

    test food truck names written

--- test/event_test.rb
+++ test/event_test.rb
@@ -41,6 +41,22 @@ class EventTest < Minitest::Test
     @event.add_food_truck(@food_truck3)
 
     assert_equal [@food_truck1, @food_truck2, @food_truck3] ,@event.food_trucks
+  end
+
+  def test_food_truck_names
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+
+    @food_truck3.stock(@item1, 65)
+
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    assert_equal [@food_truck1, @food_truck2, @food_truck3] ,@event.food_trucks
     assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @event.food_truck_names
   end
 
