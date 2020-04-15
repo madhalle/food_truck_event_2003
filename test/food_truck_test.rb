049eaed5c901db55985e4d59d456e05fc53f50f9
Mad Hallé <mad.halle@icloud.com>
2020-04-15 10:15:26 -0600

    check stock test written

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -6,6 +6,8 @@ require './lib/food_truck'
 class FoodTruckTest < Minitest::Test
   def setup
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   def test_it_exists
@@ -16,21 +18,19 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", @food_truck.name
     assert_equal ({}), @food_truck.inventory
   end
+
+  def test_check_stock
+    assert_equal 0, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item1, 30)
+    expected = {@item1 => 30}
+    assert_equal expected, @food_truck.inventory
+    assert_equal 30, @food_truck.check_stock(@item1)
+  end
 end
 
 
 
-#
-# pry(main)> food_truck.check_stock(item1)
-# #=> 0
-#
-# pry(main)> food_truck.stock(item1, 30)
-#
-# pry(main)> food_truck.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 30}
-#
-# pry(main)> food_truck.check_stock(item1)
-# #=> 30
+
 #
 # pry(main)> food_truck.stock(item1, 25)
 #
